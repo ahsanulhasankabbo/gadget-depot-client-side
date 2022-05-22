4e61624cfe61ff3c3d78e5265b35a14b5d3a53d0
Ahsanul Hasan Kabbo <kabboahsanulhasan@gmail.com>
2022-05-22 20:14:42 +0600

    navbar add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Blogs/Blogs';
+import Banner from './Pages/Home/Banner';
+import Header from './Pages/Shared/Header';
 
 function App() {
   return (
     <div>
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Banner></Banner>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+      </Routes>
     </div>
   );
 }
